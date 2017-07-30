d0b7626ddfaa8d15200f7c7463ae45d327cffa96
Gary Bacon <hello@garybacon.com>
2017-07-30 17:29:34 -0400

    Update to the Tactics dashboard design. Introducing levels child theme.

---
+++ wp-content/themes/levels-wp-1-child/template-parts/content-dark-header.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package levels
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="header header--wp header--centered header--dark">
+		<?php the_title( '<h1 class="header__title header--centered header--bright">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div id="inner" class="container--normal">
+
+	<div class="entry-content">
+		<?php
+			the_content();
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'levels' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php
+			// edit_post_link(
+			// 	sprintf(
+			// 		/* translators: %s: Name of current post */
+			// 		esc_html__( 'Edit %s', 'levels' ),
+			// 		the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			// 	),
+			// 	'<span class="edit-link">',
+			// 	'</span>'
+			// );
+		?>
+	</footer><!-- .entry-footer -->
+
+	</div><!-- #inner -->
+</article><!-- #post-## -->
