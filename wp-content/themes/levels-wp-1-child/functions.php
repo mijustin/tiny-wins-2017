5710aceaafaf76bb2355b20e427edae86121cec8
Gary Bacon <hello@garybacon.com>
2017-08-05 11:03:01 -0400

    Shortcode Troubleshooting

--- wp-content/themes/levels-wp-1-child/functions.php
+++ wp-content/themes/levels-wp-1-child/functions.php
@@ -1,7 +1,4 @@
 <?php
-/* Add some delicious shortcodes. */
-include('shortcodes.php');
-
 /* Pull in the parent theme's CSS and enqueue the child theme's CSS. */
 
 function levels_child_enqueue_styles() {
@@ -16,4 +13,7 @@ function levels_child_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'levels_child_enqueue_styles' );
+
+/* Add some delicious shortcodes. */
+include('shortcodes.php');
 ?>
--- wp-content/themes/levels-wp-1-child/shortcodes.php
+++ wp-content/themes/levels-wp-1-child/shortcodes.php
@@ -2,15 +2,12 @@
   /* Shortcodes for the Tactics Dashboard
    * Dependant on the WPcomplete plugin, which has to be activated.
    */
-
    // [course] shortcode
-   function shortcode_Course($content) {
+   function shortcode_course( $atts, $content = null ) {
 
      // create course
      return
-       '<div class="course-contain"><div class="course">' .
-       do_shortcode($content) .
-       '</div></div>';
+       '<div class="course-contain"><div class="course">' . do_shortcode($content) . '</div></div>';
    }
-   add_shortcode('course', 'shortcode_Course');
+   add_shortcode('course', 'shortcode_course');
  ?>
