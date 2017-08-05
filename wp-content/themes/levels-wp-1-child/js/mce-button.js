6e8b3af7d8f484cff3204a9acdcfb069bcf081e0
Gary Bacon <hello@garybacon.com>
2017-08-05 17:22:22 -0400

    Add icon to Visual Editor

--- wp-content/themes/levels-wp-1-child/js/mce-button.js
+++ wp-content/themes/levels-wp-1-child/js/mce-button.js
@@ -4,7 +4,7 @@
 	tinymce.PluginManager.add('levels_child_mce_button', function( editor, url ) {
 		editor.addButton('levels_child_mce_button', {
 			text: 'Levels Child',
-			icon: false,
+			icon: 'img/layout.svg',
 			onclick: function() {
         editor.focus();
 				editor.selection.setContent('[course]' + editor.selection.getContent() + '[/course]');
