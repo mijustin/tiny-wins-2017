20a4c8dd823a867a721aec757a6821a37470dd87
Gary Bacon <hello@garybacon.com>
2017-08-05 17:46:38 -0400

    Restore shortcode icon class.

--- wp-content/themes/levels-wp-1-child/js/mce-button.js
+++ wp-content/themes/levels-wp-1-child/js/mce-button.js
@@ -4,7 +4,7 @@
 	tinymce.PluginManager.add('levels_child_mce_button', function( editor, url ) {
 		editor.addButton('levels_child_mce_button', {
 			text: 'Levels Child',
-			icon: 'dashicons-layout',
+			icon: 'levels-child-icon',
 			onclick: function() {
         editor.focus();
 				editor.selection.setContent('[course]' + editor.selection.getContent() + '[/course]');
