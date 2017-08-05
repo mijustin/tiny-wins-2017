7b316e81fc75a360ac50851a257353343a173566
Gary Bacon <hello@garybacon.com>
2017-08-05 19:57:08 -0400

    Remove text label from button.

--- wp-content/themes/levels-wp-1-child/js/mce-button.js
+++ wp-content/themes/levels-wp-1-child/js/mce-button.js
@@ -3,7 +3,6 @@
 (function() {
 	tinymce.PluginManager.add('levels_child_mce_button', function( editor, url ) {
 		editor.addButton('levels_child_mce_button', {
-			text: 'Add Course',
 			icon: 'levels-child-icon',
 			onclick: function() {
         editor.focus();
