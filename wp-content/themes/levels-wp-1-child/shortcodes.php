3c6c6bab4c6a896ea0d8b30330a404f9e9f0c78a
Gary Bacon <hello@garybacon.com>
2017-08-05 13:05:55 -0400

    Added a course container shortcode.

--- wp-content/themes/levels-wp-1-child/shortcodes.php
+++ wp-content/themes/levels-wp-1-child/shortcodes.php
@@ -2,12 +2,22 @@
   /* Shortcodes for the Tactics Dashboard
    * Dependant on the WPcomplete plugin, which has to be activated.
    */
+
+   // [course-contain] shortcode
+   function shortcode_course_contain( $atts, $content = null ) {
+
+     // create course container
+     return
+        '<div class="course-contain">' . do_shortcode($content) . '</div>';
+   }
+   add_shortcode('course-contain', 'shortcode_course_contain');
+
    // [course] shortcode
    function shortcode_course( $atts, $content = null ) {
 
      // create course
      return
-       '<div class="course-contain"><div class="course">' . do_shortcode($content) . '</div></div>';
+       '<div class="course">' . do_shortcode($content) . '</div>';
    }
    add_shortcode('course', 'shortcode_course');
  ?>
