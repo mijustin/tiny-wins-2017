b53bd231d1659c02685653ed8698d1f64f5f4e70
Gary Bacon <hello@garybacon.com>
2017-08-05 10:44:19 -0400

    Course Shortcode
    Create course shortcode.

--- wp-content/themes/levels-wp-1-child/shortcodes.php
+++ wp-content/themes/levels-wp-1-child/shortcodes.php
@@ -3,5 +3,14 @@
    * Dependant on the WPcomplete plugin, which has to be activated.
    */
 
+   // [course] shortcode
+   function shortcode_Course($content) {
 
+     // create course
+     return
+       '<div class="course-contain"><div class="course">' .
+       do_shortcode($content) .
+       '</div></div>';
+   }
+   add_shortcode('course', 'shortcode_Course');
  ?>
