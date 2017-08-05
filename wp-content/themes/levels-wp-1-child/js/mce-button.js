63207ffd5ad3c7f60ba78773b4f0eb0cbe84361c
Gary Bacon <hello@garybacon.com>
2017-08-05 19:53:29 -0400

    Troubleshooting icon.

--- wp-content/themes/levels-wp-1-child/js/mce-button.js
+++ wp-content/themes/levels-wp-1-child/js/mce-button.js
@@ -4,7 +4,7 @@
 	tinymce.PluginManager.add('levels_child_mce_button', function( editor, url ) {
 		editor.addButton('levels_child_mce_button', {
 			text: 'Add Course',
-			icon: 'add-course-icon',
+			icon: 'levels-child-icon',
 			onclick: function() {
         editor.focus();
 				editor.selection.setContent('[course]' + editor.selection.getContent() + '[/course]');
